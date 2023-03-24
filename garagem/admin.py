3d41670c02ab4591e0ea310c86dc0f58fd89f291
Ruidevin <rafaelalvaro00@gmail.com>
2023-03-24 15:00:32 -0300

    VeiculosdaGaragem

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from garagem.models import Categoria, Marca, Cor, Acessorio
+from garagem.models import Categoria, Marca, Cor, Acessorio, Veiculo
 
 admin.site.register(Categoria)
 admin.site.register(Marca)
 admin.site.register(Acessorio)
 admin.site.register(Cor)
+admin.site.register(Veiculo)
+
 
 
