5395c5ba8b126dfccd2726f467ceedda4ce0ec6a
Ruidevin <rafaelalvaro00@gmail.com>
2023-03-24 14:24:40 -0300

    Marca

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from garagem.models import Categoria
+from garagem.models import Categoria, Marca
 
 admin.site.register(Categoria)
+admin.site.register(Marca)
+
--- garagem/models.py
+++ garagem/models.py
@@ -6,4 +6,10 @@ class Categoria(models.Model):
     def __str__(self):
         return self.descricao
 
+class Marca(models.Model):
+    nome = models.CharField(max_length=50)
+    nacionalidade = models.CharField(max_length=50)
+
+def __str__(self):
+    return self.nome.upper()
 
