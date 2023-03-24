9ce5fc2b52bd16f84246099f628310d820c5af8b
Ruidevin <rafaelalvaro00@gmail.com>
2023-03-24 14:55:06 -0300

    Veiculo

--- garagem/models.py
+++ garagem/models.py
@@ -25,3 +25,15 @@ class Cor(models.Model):
 def __str__(self):
     return self.descricao
 
+
+class Veiculo(models.Model):
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    cor = models.ForeignKey(Cor, on_delete=models.CASCADE)
+    ano = models.IntegerField(null=True, blank=True)
+    preco = models.DecimalField(max_digits=10,decimal_places=2, null=True default=0)
+
+def __str__(self):
+    return f"{self.marca.nome} - {self.categoria.nome} - {self.ano} - {self.cor.descricao}"
+
+
