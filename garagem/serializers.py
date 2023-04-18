66b9832ff3332526d57a4c3f9e24822719bc7dc1
RuiDevin <rafaelalvaro00@gmail.com>
2023-04-18 15:19:44 -0300

    Adição de cores

--- garagem/serializers.py
+++ garagem/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 
-from garagem.models import Marca, Categoria
+from garagem.models import Marca, Categoria, Cor
 
 class MarcaSerializer(ModelSerializer):
     class Meta:
@@ -9,5 +9,10 @@ class MarcaSerializer(ModelSerializer):
 class CategoriaSerializer(ModelSerializer):
     class Meta:
         model = Categoria
+        fields = "__all__"
+
+class CorSerializer(ModelSerializer):
+    class Meta:
+        model = Cor
         fields = "__all__"
 
